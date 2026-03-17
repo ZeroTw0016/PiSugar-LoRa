e31f2050cd0ab293cf23d9ab566c7f3f8ea7fca0
ZeroTw0016 <thezerotwo016@gmail.com>
2026-03-17 09:55:46 +0100

    init

---
+++ pisugar_server.py
@@ -0,0 +1,382 @@
+from flask import Flask, jsonify, request, abort, render_template
+import smbus2
+import time
+
+# PiSugar 3 Standard I2C-Adresse
+PISUGAR_I2C_ADDR = 0x57
+I2C_BUS = 1
+
+app = Flask(__name__)
+
+def get_bus():
+    return smbus2.SMBus(I2C_BUS)
+
+def read_register(addr, reg):
+    with get_bus() as bus:
+        return bus.read_byte_data(addr, reg)
+
+def write_register(addr, reg, value):
+    with get_bus() as bus:
+        bus.write_byte_data(addr, reg, value)
+
+def is_write_protection_on():
+    val = read_register(PISUGAR_I2C_ADDR, 0x0B)
+    return val != 1
+
+def set_write_protection(enabled: bool):
+    if enabled:
+        write_register(PISUGAR_I2C_ADDR, 0x0B, 0xFF)
+    else:
+        write_register(PISUGAR_I2C_ADDR, 0x0B, 0x29)
+    time.sleep(0.05)
+
+def get_status():
+    reg2 = read_register(PISUGAR_I2C_ADDR, 0x02)
+    reg3 = read_register(PISUGAR_I2C_ADDR, 0x03)
+    reg4 = read_register(PISUGAR_I2C_ADDR, 0x04)
+    reg6 = read_register(PISUGAR_I2C_ADDR, 0x06)
+    reg7 = read_register(PISUGAR_I2C_ADDR, 0x07)
+    reg9 = read_register(PISUGAR_I2C_ADDR, 0x09)
+    reg20 = read_register(PISUGAR_I2C_ADDR, 0x20)
+    reg22 = read_register(PISUGAR_I2C_ADDR, 0x22)
+    reg23 = read_register(PISUGAR_I2C_ADDR, 0x23)
+    reg2a = read_register(PISUGAR_I2C_ADDR, 0x2A)
+    reg40 = read_register(PISUGAR_I2C_ADDR, 0x40)
+    reg44 = [read_register(PISUGAR_I2C_ADDR, r) for r in range(0x44, 0x48)]
+    reg50 = read_register(PISUGAR_I2C_ADDR, 0x50)
+    rege0 = read_register(PISUGAR_I2C_ADDR, 0xE0)
+    return {
+        'external_power': bool((reg2 >> 7) & 1),
+        'charging_switch': bool((reg2 >> 6) & 1),
+        'output_switch': bool((reg2 >> 2) & 1),
+        'output_switch_delay': bool((reg2 >> 5) & 1),
+        'output_switch_delay_time': reg9,
+        'auto_resume_boot': bool((reg2 >> 4) & 1),
+        'anti_mistouch': bool((reg2 >> 3) & 1),
+        'button_pressed': bool(reg2 & 1),
+        'auto_hibernate': bool((reg3 >> 6) & 1),
+        'soft_shutdown': bool((reg3 >> 4) & 1),
+        'soft_shutdown_state': bool((reg3 >> 3) & 1),
+        'chip_temp_c': reg4 - 40,
+        'watchdog_enabled': bool((reg6 >> 7) & 1),
+        'watchdog_reset': bool((reg6 >> 5) & 1),
+        'watchdog_interval': reg7,
+        'boot_watchdog_enabled': bool((reg6 >> 4) & 1),
+        'boot_watchdog_reset': bool((reg6 >> 3) & 1),
+        'boot_watchdog_restart_limit': read_register(PISUGAR_I2C_ADDR, 0x0A),
+        'charging_protection': bool((reg20 >> 7) & 1),
+        'scl_awake': bool((reg20 >> 3) & 1),
+        'battery_voltage_mv': ((reg22 << 8) | reg23),
+        'battery_percent': reg2a,
+        'timing_boot_enabled': bool((reg40 >> 7) & 1),
+        'timing_boot_data': reg44,
+        'custom_i2c_addr': reg50,
+        'led_control': rege0 & 0x0F,
+        'write_protection': is_write_protection_on()
+    }
+
+
+# Status-Endpunkt
+@app.route('/api/status', methods=['GET'])
+def api_status():
+    try:
+        return jsonify(get_status())
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+# Write-Protection-Endpunkt
+@app.route('/api/write_protection', methods=['GET', 'POST'])
+def api_write_protection():
+    if request.method == 'GET':
+        return jsonify({'write_protection': is_write_protection_on()})
+    if request.method == 'POST':
+        if is_write_protection_on():
+            abort(403, description='Write protection is enabled. Changes are not allowed.')
+        data = request.get_json(force=True)
+        enabled = bool(data.get('enabled', True))
+        set_write_protection(enabled)
+        return jsonify({'write_protection': is_write_protection_on()})
+
+# Beispiel für Output-Switch (Register 0x02, Bit 2)
+# Beispiel für Output-Switch (Register 0x02, Bit 2)
+
+# --- Output Switch ---
+@app.route('/api/output_switch', methods=['GET', 'POST'])
+def api_output_switch():
+    if request.method == 'GET':
+        reg2 = read_register(PISUGAR_I2C_ADDR, 0x02)
+        return jsonify({'output_switch': bool((reg2 >> 2) & 1)})
+    if request.method == 'POST':
+        if is_write_protection_on():
+            abort(403, description='Write protection is enabled. Changes are not allowed.')
+        data = request.get_json(force=True)
+        on = bool(data.get('on', True))
+        reg2 = read_register(PISUGAR_I2C_ADDR, 0x02)
+        if on:
+            reg2 |= (1 << 2)
+        else:
+            reg2 &= ~(1 << 2)
+        write_register(PISUGAR_I2C_ADDR, 0x02, reg2)
+        return jsonify({'output_switch': bool((reg2 >> 2) & 1)})
+
+# --- Charging Switch ---
+@app.route('/api/charging_switch', methods=['GET', 'POST'])
+def api_charging_switch():
+    if request.method == 'GET':
+        reg2 = read_register(PISUGAR_I2C_ADDR, 0x02)
+        return jsonify({'charging_switch': bool((reg2 >> 6) & 1)})
+    if request.method == 'POST':
+        if is_write_protection_on():
+            abort(403, description='Write protection is enabled. Changes are not allowed.')
+        data = request.get_json(force=True)
+        on = bool(data.get('on', True))
+        reg2 = read_register(PISUGAR_I2C_ADDR, 0x02)
+        if on:
+            reg2 |= (1 << 6)
+        else:
+            reg2 &= ~(1 << 6)
+        write_register(PISUGAR_I2C_ADDR, 0x02, reg2)
+        return jsonify({'charging_switch': bool((reg2 >> 6) & 1)})
+
+# --- Output Switch Delay ---
+@app.route('/api/output_switch_delay', methods=['POST'])
+def api_output_switch_delay():
+    if is_write_protection_on():
+        abort(403, description='Write protection is enabled. Changes are not allowed.')
+    data = request.get_json(force=True)
+    delay = int(data.get('delay', 0))
+    write_register(PISUGAR_I2C_ADDR, 0x09, delay)
+    reg2 = read_register(PISUGAR_I2C_ADDR, 0x02)
+    reg2 &= ~(1 << 5)
+    write_register(PISUGAR_I2C_ADDR, 0x02, reg2)
+    return jsonify({'output_switch_delay_time': delay})
+
+# --- Auto Resume Boot ---
+@app.route('/api/auto_resume_boot', methods=['GET', 'POST'])
+def api_auto_resume_boot():
+    if request.method == 'GET':
+        reg2 = read_register(PISUGAR_I2C_ADDR, 0x02)
+        return jsonify({'auto_resume_boot': bool((reg2 >> 4) & 1)})
+    if request.method == 'POST':
+        if is_write_protection_on():
+            abort(403, description='Write protection is enabled. Changes are not allowed.')
+        data = request.get_json(force=True)
+        on = bool(data.get('on', True))
+        reg2 = read_register(PISUGAR_I2C_ADDR, 0x02)
+        if on:
+            reg2 |= (1 << 4)
+        else:
+            reg2 &= ~(1 << 4)
+        write_register(PISUGAR_I2C_ADDR, 0x02, reg2)
+        return jsonify({'auto_resume_boot': bool((reg2 >> 4) & 1)})
+
+# --- Anti-Mistouch ---
+@app.route('/api/anti_mistouch', methods=['GET', 'POST'])
+def api_anti_mistouch():
+    if request.method == 'GET':
+        reg2 = read_register(PISUGAR_I2C_ADDR, 0x02)
+        return jsonify({'anti_mistouch': bool((reg2 >> 3) & 1)})
+    if request.method == 'POST':
+        if is_write_protection_on():
+            abort(403, description='Write protection is enabled. Changes are not allowed.')
+        data = request.get_json(force=True)
+        on = bool(data.get('on', True))
+        reg2 = read_register(PISUGAR_I2C_ADDR, 0x02)
+        if on:
+            reg2 |= (1 << 3)
+        else:
+            reg2 &= ~(1 << 3)
+        write_register(PISUGAR_I2C_ADDR, 0x02, reg2)
+        return jsonify({'anti_mistouch': bool((reg2 >> 3) & 1)})
+
+# --- Auto Hibernate ---
+@app.route('/api/auto_hibernate', methods=['GET', 'POST'])
+def api_auto_hibernate():
+    if request.method == 'GET':
+        reg3 = read_register(PISUGAR_I2C_ADDR, 0x03)
+        return jsonify({'auto_hibernate': bool((reg3 >> 6) & 1)})
+    if request.method == 'POST':
+        if is_write_protection_on():
+            abort(403, description='Write protection is enabled. Changes are not allowed.')
+        data = request.get_json(force=True)
+        on = bool(data.get('on', True))
+        reg3 = read_register(PISUGAR_I2C_ADDR, 0x03)
+        if on:
+            reg3 |= (1 << 6)
+        else:
+            reg3 &= ~(1 << 6)
+        write_register(PISUGAR_I2C_ADDR, 0x03, reg3)
+        return jsonify({'auto_hibernate': bool((reg3 >> 6) & 1)})
+
+# --- Soft Shutdown ---
+@app.route('/api/soft_shutdown', methods=['GET', 'POST'])
+def api_soft_shutdown():
+    if request.method == 'GET':
+        reg3 = read_register(PISUGAR_I2C_ADDR, 0x03)
+        return jsonify({'soft_shutdown': bool((reg3 >> 4) & 1)})
+    if request.method == 'POST':
+        if is_write_protection_on():
+            abort(403, description='Write protection is enabled. Changes are not allowed.')
+        data = request.get_json(force=True)
+        on = bool(data.get('on', True))
+        reg3 = read_register(PISUGAR_I2C_ADDR, 0x03)
+        if on:
+            reg3 |= (1 << 4)
+        else:
+            reg3 &= ~(1 << 4)
+        write_register(PISUGAR_I2C_ADDR, 0x03, reg3)
+        return jsonify({'soft_shutdown': bool((reg3 >> 4) & 1)})
+
+# --- Watchdog ---
+@app.route('/api/watchdog', methods=['GET', 'POST'])
+def api_watchdog():
+    if request.method == 'GET':
+        reg6 = read_register(PISUGAR_I2C_ADDR, 0x06)
+        reg7 = read_register(PISUGAR_I2C_ADDR, 0x07)
+        return jsonify({
+            'watchdog_enabled': bool((reg6 >> 7) & 1),
+            'watchdog_reset': bool((reg6 >> 5) & 1),
+            'watchdog_interval': reg7
+        })
+    if request.method == 'POST':
+        if is_write_protection_on():
+            abort(403, description='Write protection is enabled. Changes are not allowed.')
+        data = request.get_json(force=True)
+        reg6 = read_register(PISUGAR_I2C_ADDR, 0x06)
+        if 'enabled' in data:
+            if data['enabled']:
+                reg6 |= (1 << 7)
+            else:
+                reg6 &= ~(1 << 7)
+        if 'reset' in data and data['reset']:
+            reg6 |= (1 << 5)
+        write_register(PISUGAR_I2C_ADDR, 0x06, reg6)
+        if 'interval' in data:
+            write_register(PISUGAR_I2C_ADDR, 0x07, int(data['interval']))
+        return jsonify({'success': True})
+
+# --- Boot Watchdog ---
+@app.route('/api/boot_watchdog', methods=['GET', 'POST'])
+def api_boot_watchdog():
+    if request.method == 'GET':
+        reg6 = read_register(PISUGAR_I2C_ADDR, 0x06)
+        return jsonify({
+            'boot_watchdog_enabled': bool((reg6 >> 4) & 1),
+            'boot_watchdog_reset': bool((reg6 >> 3) & 1),
+            'boot_watchdog_restart_limit': read_register(PISUGAR_I2C_ADDR, 0x0A)
+        })
+    if request.method == 'POST':
+        if is_write_protection_on():
+            abort(403, description='Write protection is enabled. Changes are not allowed.')
+        data = request.get_json(force=True)
+        reg6 = read_register(PISUGAR_I2C_ADDR, 0x06)
+        if 'enabled' in data:
+            if data['enabled']:
+                reg6 |= (1 << 4)
+            else:
+                reg6 &= ~(1 << 4)
+        if 'reset' in data and data['reset']:
+            reg6 |= (1 << 3)
+        write_register(PISUGAR_I2C_ADDR, 0x06, reg6)
+        if 'restart_limit' in data:
+            write_register(PISUGAR_I2C_ADDR, 0x0A, int(data['restart_limit']))
+        return jsonify({'success': True})
+
+# --- Charging Protection ---
+@app.route('/api/charging_protection', methods=['GET', 'POST'])
+def api_charging_protection():
+    if request.method == 'GET':
+        reg20 = read_register(PISUGAR_I2C_ADDR, 0x20)
+        return jsonify({'charging_protection': bool((reg20 >> 7) & 1)})
+    if request.method == 'POST':
+        if is_write_protection_on():
+            abort(403, description='Write protection is enabled. Changes are not allowed.')
+        data = request.get_json(force=True)
+        on = bool(data.get('on', True))
+        reg20 = read_register(PISUGAR_I2C_ADDR, 0x20)
+        if on:
+            reg20 |= (1 << 7)
+        else:
+            reg20 &= ~(1 << 7)
+        write_register(PISUGAR_I2C_ADDR, 0x20, reg20)
+        return jsonify({'charging_protection': bool((reg20 >> 7) & 1)})
+
+# --- SCL Awake ---
+@app.route('/api/scl_awake', methods=['GET', 'POST'])
+def api_scl_awake():
+    if request.method == 'GET':
+        reg20 = read_register(PISUGAR_I2C_ADDR, 0x20)
+        return jsonify({'scl_awake': bool((reg20 >> 3) & 1)})
+    if request.method == 'POST':
+        if is_write_protection_on():
+            abort(403, description='Write protection is enabled. Changes are not allowed.')
+        data = request.get_json(force=True)
+        on = bool(data.get('on', True))
+        reg20 = read_register(PISUGAR_I2C_ADDR, 0x20)
+        if on:
+            reg20 |= (1 << 3)
+        else:
+            reg20 &= ~(1 << 3)
+        write_register(PISUGAR_I2C_ADDR, 0x20, reg20)
+        return jsonify({'scl_awake': bool((reg20 >> 3) & 1)})
+
+# --- Timing Boot ---
+@app.route('/api/timing_boot', methods=['GET', 'POST'])
+def api_timing_boot():
+    if request.method == 'GET':
+        reg40 = read_register(PISUGAR_I2C_ADDR, 0x40)
+        reg44 = [read_register(PISUGAR_I2C_ADDR, r) for r in range(0x44, 0x48)]
+        return jsonify({'timing_boot_enabled': bool((reg40 >> 7) & 1), 'timing_boot_data': reg44})
+    if request.method == 'POST':
+        if is_write_protection_on():
+            abort(403, description='Write protection is enabled. Changes are not allowed.')
+        data = request.get_json(force=True)
+        reg40 = read_register(PISUGAR_I2C_ADDR, 0x40)
+        if 'enabled' in data:
+            if data['enabled']:
+                reg40 |= (1 << 7)
+            else:
+                reg40 &= ~(1 << 7)
+            write_register(PISUGAR_I2C_ADDR, 0x40, reg40)
+        if 'data' in data and isinstance(data['data'], list) and len(data['data']) == 4:
+            for i, v in enumerate(data['data']):
+                write_register(PISUGAR_I2C_ADDR, 0x44 + i, int(v))
+        return jsonify({'success': True})
+
+# --- Custom I2C Address ---
+@app.route('/api/custom_i2c_addr', methods=['GET', 'POST'])
+def api_custom_i2c_addr():
+    if request.method == 'GET':
+        reg50 = read_register(PISUGAR_I2C_ADDR, 0x50)
+        return jsonify({'custom_i2c_addr': reg50})
+    if request.method == 'POST':
+        if is_write_protection_on():
+            abort(403, description='Write protection is enabled. Changes are not allowed.')
+        data = request.get_json(force=True)
+        addr = int(data.get('addr', 0x57))
+        write_register(PISUGAR_I2C_ADDR, 0x50, addr)
+        return jsonify({'custom_i2c_addr': addr})
+
+# --- LED Control ---
+@app.route('/api/led_control', methods=['GET', 'POST'])
+def api_led_control():
+    if request.method == 'GET':
+        rege0 = read_register(PISUGAR_I2C_ADDR, 0xE0)
+        return jsonify({'led_control': rege0 & 0x0F})
+    if request.method == 'POST':
+        if is_write_protection_on():
+            abort(403, description='Write protection is enabled. Changes are not allowed.')
+        data = request.get_json(force=True)
+        value = int(data.get('value', 0)) & 0x0F
+        rege0 = read_register(PISUGAR_I2C_ADDR, 0xE0)
+        rege0 = (rege0 & 0xF0) | value
+        write_register(PISUGAR_I2C_ADDR, 0xE0, rege0)
+        return jsonify({'led_control': value})
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
